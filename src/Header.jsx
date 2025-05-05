d1525d7b57b86e64f838f335d1aba5a95cd1c0ea
Vinícius Dias <diasvinicius0401@gmail.com>
2025-05-05 10:29:13 -0300

    feat: header component and adjust tailwindcss

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="flex p-6 justify-center text-xl font-bold">
+      To Do List
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/ToDo.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function ToDo() {
+  return (
+    <div className="bg-slate-100 min-h-screen">
+      <Header />
+    </div>
+  );
+}
+
+export default ToDo;
