56dfe96e7fefe8efbe2b5088c5d525d91eaa2605
Vinícius Dias <diasvinicius0401@gmail.com>
2025-05-08 20:16:23 -0300

    feat: Adiciona funções para exclusão e checked

--- src/List.jsx
+++ src/List.jsx
@@ -1,7 +1,7 @@
 import { FaCheckCircle } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
 
-function List({ items }) {
+function List({ items, onDeleteItem, onCheckItem }) {
   return (
     <ul className="flex flex-col items px-8 rounded-md gap-3">
       {items.map((item) => (
@@ -9,10 +9,16 @@ function List({ items }) {
           <div className="w-full bg-white flex justify-between gap-2 items-center rounded-md px-4 py-2">
             <p>{item.text}</p>
             <div className="flex gap-2">
-              <button className="rounded-full p-1.5 hover:bg-black hover:text-white">
-                <FaCheckCircle />
+              <button
+                className="rounded-full p-1.5 hover:bg-black hover:text-white"
+                onClick={() => onCheckItem(item.id)}
+              >
+                {item.checked ? <FaCheckCircle /> : "feito?"}
               </button>
-              <button className="rounded-full p-1.5 hover:bg-black hover:text-white">
+              <button
+                className="rounded-full p-1.5 hover:bg-black hover:text-white"
+                onClick={() => onDeleteItem(item.id)}
+              >
                 <FaTrashAlt />
               </button>
             </div>
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -12,12 +12,28 @@ function ToDo() {
     setItems([...items, newItem]);
   };
 
+  const onCheckItem = (id) => {
+    const updatedItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(updatedItems);
+  };
+
+  const onDeleteItem = (id) => {
+    const filteredItems = items.filter((item) => item.id !== id);
+    setItems(filteredItems);
+  };
+
   return (
     <div className="bg-slate-100 min-h-screen">
       <div className="max-w-[900px] m-auto">
         <Header />
         <ToDoForm addItem={AddItem} />
-        <List items={items} />
+        <List
+          items={items}
+          onCheckItem={onCheckItem}
+          onDeleteItem={onDeleteItem}
+        />
       </div>
     </div>
   );
